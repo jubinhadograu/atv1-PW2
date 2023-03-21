1557c2b13b021abde3d259d8e7d994a736ff54c3
jubinhadograu <juliasrly@gmail.com>
2023-03-20 22:01:23 -0300

    teste 1

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+app.use(express.urlencoded({extended:true}));
+
+app.listen(3000, ()=>{
+    console.log('SERVIDOR RODANDO EM - http://localhost:3000');
+});
